16a70c0226829844d46241fd55ae61b79dad29ff
MaxRau <max.rauchaplin@gmail.com>
2015-06-09 12:55:23 -0300

    Added listeners for buttons, some Todos regarding where code for favoriting/skipping functions should go.

--- mobile/src/main/java/ubicom/com/fitunes/MainActivityFragment.java
+++ mobile/src/main/java/ubicom/com/fitunes/MainActivityFragment.java
@@ -5,19 +5,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.ImageButton;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment{
 
     public MainActivityFragment() {
     }
-
+    View rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ImageButton buttonUp = (ImageButton) rootView.findViewById(R.id.buttonUp);
+        buttonUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                onButtonUp(v);
+            }
+        });
+
+        ImageButton buttonDown = (ImageButton) rootView.findViewById(R.id.buttonDown);
+        buttonDown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonDown(v);
+            }
+        });
+
+        return rootView;
+    }
+    public void onButtonUp(View v)
+    {
+        //Todo Add Favorite Function on Click
+        Toast.makeText(getActivity(), "Up Button Pressed!", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButtonDown(View v)
+    {
+       //Todo Add Skip Function on Click
+        Toast.makeText(getActivity(), "Down Button Pressed!", Toast.LENGTH_LONG).show();
     }
 }
